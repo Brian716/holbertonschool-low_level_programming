9fdc3caf393cc7896133d56fec6f335166922901
brian176 <brian.edu25@gmail.com>
2021-06-18 12:00:41 -0700

    hexadecimal symbols

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function prints exact phrases
+ * Return: 0
+ */
+
+int main(void)
+{
+int A = 48;
+int B = 97;
+while (A <= 57)
+{
+putchar(A);
+A++;
+}
+while (B <= 102)
+{
+putchar(B);
+B++;
+}
+putchar('\n');
+return (0);
+}
