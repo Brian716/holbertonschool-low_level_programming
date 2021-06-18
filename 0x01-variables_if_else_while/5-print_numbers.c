d135d45f156267aaa54049b2a3eb0303ffa02177
brian176 <brian.edu25@gmail.com>
2021-06-18 11:25:57 -0700

    while and printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function prints exact phrases
+ * Return: 0
+ */
+
+int main(void)
+{
+int A = 0;
+while (A < 10)
+{
+printf("%d", A);
+A++;
+}
+putchar('\n');
+return (0);
+}
