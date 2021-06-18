6e37d0e85f2ec7a291704c1fe602c29174dc7000
brian176 <brian.edu25@gmail.com>
2021-06-18 10:10:52 -0700

    if else and while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function prints exact phrases
+ * Return: 0
+ */
+
+int main(void)
+{
+int A = 97;
+while (A <= 122)
+{
+putchar(A);
+A++;
+}
+putchar('\n');
+return (0);
+}
