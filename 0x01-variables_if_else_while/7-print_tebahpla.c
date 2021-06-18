13d6fd279e1c9143dbeedab7607356020c2f2b33
brian176 <brian.edu25@gmail.com>
2021-06-18 11:52:44 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function prints exact phrases
+ * Return: 0
+ */
+
+int main(void)
+{
+int A = 122;
+while (A >= 97)
+{
+putchar(A);
+A--;
+}
+putchar('\n');
+return (0);
+}
